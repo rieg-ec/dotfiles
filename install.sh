872545e3fde39cfa249064b62d686faff9cce2df
rieg-ec <rieg@uc.cl>
2021-05-18 13:40:04 -0400

    update install script

--- install.sh
+++ install.sh
@@ -31,10 +31,6 @@ echo "installing node..."
 NVM_DOWNLOAD_LINK=https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh
 curl -o- NVM_DOWNLOAD_LINK | bash
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 nvm install v15
 nvm cache clear
 echo "node $(node --version) installed" >> $log_file
