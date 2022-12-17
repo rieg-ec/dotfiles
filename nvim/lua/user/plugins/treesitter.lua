51e0e14f9008fc1f42899fc35cc52ce2d32b8167
rieg-ec <rieg@uc.cl>
2022-12-17 20:58:16 -0300

    fix(nvim/treesitter): ignore phpdoc parser

--- nvim/lua/user/plugins/treesitter.lua
+++ nvim/lua/user/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ignore_install = { "phpdoc" },
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   highlight = {
     enable = true, -- false will disable the whole extension
