b59929912a0906060c0b39138673271be2f2171d
rieg-ec <rieg.10199@gmail.com>
2026-03-09 13:12:48 -0300

    fix: linter format

--- nvim/lua/user/lsp/conform.lua
+++ nvim/lua/user/lsp/conform.lua
@@ -5,10 +5,10 @@ require("conform").setup({
   formatters_by_ft = {
     -- Ruby - handled by RuboCop LSP (see languages/ruby.lua)
     -- ruby = {},
-    
+
     -- Python - use black
     python = { "black" },
-    
+
     -- JavaScript/TypeScript/Vue - ESLint handles formatting via LSP
     -- (formatting is done by ESLint LSP on save, see lspconfig.lua)
     -- javascript = {},
@@ -16,29 +16,29 @@ require("conform").setup({
     -- typescript = {},
     -- typescriptreact = {},
     -- vue = {},
-    
+
     -- JSON/YAML - no formatter (let LSP handle it)
     -- json = {},
     -- yaml = {},
-    
+
     -- Go
     go = { "gofmt" },
-    
+
     -- Rust
     rust = { "rustfmt" },
-    
+
     -- C/C++
     c = { "clang_format" },
     cpp = { "clang_format" },
-    
+
     -- Shell
     sh = { "shfmt" },
     bash = { "shfmt" },
-    
+
     -- Lua
     lua = { "stylua" },
   },
-  
+
   -- Format on save
   format_on_save = function(bufnr)
     -- Disable format on save for large files
@@ -47,13 +47,13 @@ require("conform").setup({
     if ok and stats and stats.size > max_filesize then
       return
     end
-    
+
     return {
-      timeout_ms = 3000,  -- Increased timeout for RuboCop on Rails files
+      timeout_ms = 3000, -- Increased timeout for RuboCop on Rails files
       lsp_fallback = true,
     }
   end,
-  
+
   -- Customize formatters
   formatters = {
     black = {
@@ -66,13 +66,13 @@ require("conform").setup({
 vim.keymap.set({ "n", "v" }, "<leader>F", function()
   local bufnr = vim.api.nvim_get_current_buf()
   local filetype = vim.bo[bufnr].filetype
-  
+
   -- For Ruby files, use LSP formatting (RuboCop LSP)
   if filetype == "ruby" then
     vim.notify("Formatting Ruby file with RuboCop LSP...", vim.log.levels.INFO)
     vim.lsp.buf.format({
       async = false,
-      timeout_ms = 5000,  -- Give RuboCop enough time for large Rails files
+      timeout_ms = 5000, -- Give RuboCop enough time for large Rails files
       bufnr = bufnr,
       filter = function(client)
         -- Use only RuboCop LSP for formatting Ruby files
@@ -97,4 +97,3 @@ vim.keymap.set({ "n", "v" }, "<leader>F", function()
     end)
   end
 end, { desc = "Format file or range (in visual mode)" })
-
