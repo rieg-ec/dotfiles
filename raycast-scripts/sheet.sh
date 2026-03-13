cfb66b78fb2b6c786301e2e6e98cfce8ecf37d85
rieg-ec <rieg.10199@gmail.com>
2026-03-13 00:57:52 -0300

    feat: add Raycast script to open Google Sheets

---
+++ raycast-scripts/sheet.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Sheet
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 📊
+
+# Documentation:
+# @raycast.description Open Google Sheets
+# @raycast.author rieg-ec
+
+open "https://docs.google.com/spreadsheets/u/1/"
