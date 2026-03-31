7064eb91044dc5cda7557e77ff086b27fb318d4b
rieg-ec <rieg.10199@gmail.com>
2026-03-31 13:37:25 -0600

    fix(ruby): skip LSP attach for octo review buffers
    Prevent Ruby LSPs from attaching to octo review pseudo-files by filtering
    them in root_dir before startup. This avoids Solargraph file lookup errors
    and prevents review buffer teardown from tripping over late client detaches.

--- nvim/lua/user/lsp/languages/ruby.lua
+++ nvim/lua/user/lsp/languages/ruby.lua
@@ -1,14 +1,36 @@
 -- Ruby LSP Configurations
 
+local root_markers = { 'Gemfile', '.git', '.rubocop.yml' }
+
+local function ruby_root_dir(bufnr, on_dir)
+  local name = vim.api.nvim_buf_get_name(bufnr)
+  if name:match('^octo://') then
+    return
+  end
+
+  local root = vim.fs.root(bufnr, root_markers)
+  if root then
+    on_dir(root)
+  end
+end
+
+local function ruby_on_attach(setup_fn)
+  return function(client, bufnr)
+    if setup_fn then
+      setup_fn(client, bufnr)
+    end
+  end
+end
+
 -- Ruby (Solargraph)
 vim.lsp.config('solargraph', {
-  on_attach = function(client, bufnr)
+  root_dir = ruby_root_dir,
+  on_attach = ruby_on_attach(function(client, bufnr)
     -- Disable Solargraph's formatting (we use RuboCop via conform.nvim instead)
     client.server_capabilities.documentFormattingProvider = false
     client.server_capabilities.documentRangeFormattingProvider = false
-  end,
+  end),
   cmd = { "/Users/rieg/.rbenv/shims/solargraph", "stdio" },
-  root_markers = { 'Gemfile', '.git', '.rubocop.yml' },
   settings = {
     solargraph = {
       autoformat = false,
@@ -22,12 +44,12 @@ vim.lsp.config('solargraph', {
 
 -- RuboCop LSP (for proper Ruby linting and formatting with project's .rubocop.yml)
 vim.lsp.config('rubocop', {
-  on_attach = function(client, bufnr)
+  root_dir = ruby_root_dir,
+  on_attach = ruby_on_attach(function(client, bufnr)
     -- Enable RuboCop's formatting capability
     client.server_capabilities.documentFormattingProvider = true
     client.server_capabilities.documentRangeFormattingProvider = true
-  end,
+  end),
   cmd = { "/Users/rieg/.rbenv/shims/rubocop", "--lsp" },
-  root_markers = { 'Gemfile', '.git', '.rubocop.yml' },
   filetypes = { "ruby" },
 })
