a08caa70cdd1872a56239180974f1184cc90d869
rieg-ec <rieg.10199@gmail.com>
2026-05-04 20:30:27 -0400

    feat(raycast): add Google Drive shortcut

---
+++ raycast-scripts/google drive.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Google Drive
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 📁
+
+# Documentation:
+# @raycast.description Open Google Drive
+# @raycast.author rieg-ec
+
+open "https://drive.google.com/drive/u/1/home"
